b7f0d5c39fbd7dc38beda441a6ecff84a0bff252
Peter Robicheaux <peter@crowdai.com>
2021-02-27 23:22:21 -0800

    Adding in ascii fonts and refactor

--- src/App.js
+++ src/App.js
@@ -1,19 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import background1 from "./img/placeholder.jpg"
+import background2 from "./img/background2.png"
+import "./App.css";
+import React, { useEffect } from "react";
+import ASCII from 'react-rainbow-ascii'
 
-function App() {
+function Block({header, body, image}) {
   return (
-    <div className="App">
-      <div className="Header-box">
+      <div className="Header-box" style={{backgroundImage: `url(${image})`}}>
         <div className="Content-box">
           <div className="App-header">
-            Hello!
-          </div>
-          <p>
-            This is smus's home page!
-          </p>
+            <ASCII text={header} rainbow= {false}/>
+            </div>
+          <p>{body}</p>
         </div>
       </div>
+  )
+}
+
+const body2 = "I (Peter) am smus. The name 'smus' is a reference to my tag in the SSBM scene. It's just generally what I go by online."
+function App() {
+  useEffect(() => {
+    document.title = "smus";
+  }, []);
+  return (
+    <div className="App">
+      <Block header = "Welcome to the smus club!" body = "You have finally arrived home, at the best site on the web" image = {background1} />
+      <Block header = "What is smus?" body = {body2} image = {background2} />
     </div>
   );
 }
